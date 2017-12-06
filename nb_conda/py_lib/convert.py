0c6f09603741e691102700e55d4d92b55ab62713
jasonych99 <jasonych99@gmail.com>
2017-12-06 13:59:13 +0100

    first complete

--- nb_conda/py_lib/convert.py
+++ nb_conda/py_lib/convert.py
@@ -1,51 +1,99 @@
 import os
 import pandas as pd
 from datetime import datetime
+from sqlalchemy.sql import text
 from .lib.database import *
 from .lib.fp_center import *
 from .lib.rdt_mapping import *
+from .lib.pickle_storage import *
 
 workspace = '/Users/jason/workspace/python/dl-platform'
 rd_folder = '/data'
+class RxnTemplate(Base):
+    __tablename__ = 'template'
+    
+    id = Column(Integer, primary_key=True)
+    rxn_identifier = Column(Integer, index=True)
+    smiles = Column(Text)
+    mapping = Column(Text)
+    center = Column(Text)
+    rct_fp = Column(Text)
+    centre_id = Column(Integer, ForeignKey('centers.id'), index=True)
 
 class Convert():
 	def raw_files(self, target, topic, method):
-		session = self.__create_db(self, target, topic, method)
-		self.__load_to_db(self, target)
-		self.__mapping(self, session)
+		# TBD return if target, topic, method none
+		target, extension = os.path.splitext(target)
+		work_path = self.__work_path(topic, method)
+		db_path = self.__db_path(work_path)
+		engine = init_engine(db_path)
+		self.__db_exits(db_path, engine)
+		self.__add_table_db(target, db_path, engine)
+		self.__load_to_db(engine, target, extension)
+		self.__mapping(engine, target, work_path)
+		self.__pickle(engine, target, work_path)
 
-	def __create_db(self, target, topic, method):
-		db_name = workspace + '/tp_%s/mt_%s/db/sql.db' % (topic, method)
-		engine = init_engine(db_name)
-		Base.metadata.create_all(engine)
-		session = init_session(engine)
-		return session
+	def __db_path(self, work_path):
+		return work_path + '/db/sql.db'
 
-	def __load_to_db(self, target):
-		csv_path = workspace + rd_folder + '/' + target
+	def __work_path(self, topic, method):
+		return workspace + '/tp_%s/mt_%s' % (topic, method)
+
+	def __db_exits(self, db_path, engine):
+		if not os.path.exists(db_path):
+			Base.metadata.create_all(engine)
+
+	def __add_table_db(self, target, db_path, engine):
+		# TBD return if check table exist
+		# TBD verify target
+		text_drop = 'DROP TABLE IF EXISTS %s' % target
+		text_create_table = 'CREATE TABLE %s(id INTEGER PRIMARY KEY, rxn_identifier TEXT, smiles TEXT, mapping Text, center Text, rct_fp Text, centre_id Integer)' % target
+		text_index = 'CREATE INDEX ix_%s_rxn_identifier ON %s(rxn_identifier)' % (target, target)
+		with engine.connect() as con:
+			con.execute(text(text_drop))
+			con.execute(text(text_create_table))
+			con.execute(text(text_index))
+			# FOREIGN KEY centre_id
+
+	def __load_to_db(self, engine, target, extension):
+		csv_path = workspace + rd_folder + '/' + target + extension
 		df_t = pd.read_csv(csv_path, header=None)
 		df_t.columns = ['smiles']
 		df_t.to_sql(
-			name=NewReaction.__tablename__, 
+			name=target, 
 			con=engine, 
 			index=False,
 			chunksize=1000,
 			if_exists='append',
 		)
+	def __load_db_config(self, engine, target):
+		detail = { '__tablename__': target, '__table_args__': { 'autoload_with': engine }, }
+		target_class = type(target, (Base,), detail)
 
-	def __mapping(self, session):
-		new_rxns = session.query(NewReaction).order_by(NewReaction.id)
-		for idx, rxn in enumerate(new_rxns):
-		    try:
-		        rdt_map(rxn.smiles)
-		        if no_xml_present(): continue
+		session = init_session(engine)
+		return [session, target_class]
 
-		        aam, centre_l1, centre_l2, centre_l3 = rdt_parse()
-		        rxn.mapping = aam
-		        rxn.center = centre_l1
-		        rm_metadata()
+	def __mapping(self, engine, target, work_path):
+		session, target_class = self.__load_db_config(engine, target)
+		rxns = session.query(target_class).order_by(target_class.id)
+		for idx, rxn in enumerate(rxns):
+			try:
+				rdt_map(rxn.smiles, work_path)
+				if no_xml_present(): continue
 
-		        session.commit()
-		    except Exception as e:
-		        print(e)
-		        continue
+				aam, centre_l1, centre_l2, centre_l3 = rdt_parse()
+				rxn.mapping = aam
+				rxn.center = centre_l1
+				rm_metadata()
+
+				session.commit()
+			except Exception as e:
+				print(e)
+				continue
+
+	def __pickle(self, engine, target, work_path):
+		session, target_class = self.__load_db_config(engine, target)
+		rxns = session.query(target_class).order_by(target_class.id)
+		data = np_fps_centers(rxns)
+		file_name = work_path + '/pickle/%s.pickle' % target
+		save_to_pickle(data, file_name)
