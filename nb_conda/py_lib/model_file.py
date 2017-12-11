12e08d1c8ccd213365e9de63e302ca82943b4594
jasonych99 <jasonych99@gmail.com>
2017-12-11 11:43:59 +0100

    upd

--- nb_conda/py_lib/model_file.py
+++ nb_conda/py_lib/model_file.py
@@ -5,11 +5,14 @@ workspace = '/Users/jason/workspace/python/dl-platform'
 class ModelFile():
 	def convert(self, json_body, topic, method):
 		model = json_body['model']
-		lib_learn = workspace + "/tp_%s/mt_%s/lib/learning/main.py" % (topic, method)
+		project_path = "/tp_%s/mt_%s" % (topic, method)
+		lib_learn_path = workspace + project_path + "/lib/learning/main.py"
+		pickle_path = workspace + project_path + "/pickle"
+		paths = model['ckdItems']
 
 		cmd = [
-			"python",
-			lib_learn,
-			" ".join(model['ckdItems'])
+			"/Users/jason/anaconda3/envs/std-dl/bin/python",
+			lib_learn_path,
+			" ".join(paths)
 		]
 		RunCmd(cmd, 1200).Run()
